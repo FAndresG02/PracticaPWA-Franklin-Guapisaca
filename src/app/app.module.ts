34bdb23e0980bf78b1620dd2c9cfb75ebc283124
Franklin G <andresfranklin02@gmail.com>
2023-11-28 22:01:34 -0500

    actualizando iconos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { EditarComponent } from './paginas/editar/editar.component';
 import { ListarComponent } from './paginas/listar/listar.component';
 import { GestionComponent } from './paginas/gestion/gestion.component';
 import { MostrarComponent } from './paginas/mostrar/mostrar.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 
@@ -31,7 +32,13 @@ import { MostrarComponent } from './paginas/mostrar/mostrar.component';
     provideFirestore(() => getFirestore()),
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
 
